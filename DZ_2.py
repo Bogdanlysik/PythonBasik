e98b17db0583e2d3c938935ad2a40ce026264e3e
bogdansevcenko <lysik8888@gmail.com>
2023-02-21 12:44:49 +0200

    Домашка-2

---
+++ DZ_2.py
@@ -0,0 +1,6 @@
+n = int(input('школярі:'))
+k = int(input('яблук:')
+apple_for_student = k // n
+apple_v_kohk = k % n
+print('кількість яблук на одного школярика:',  apple_for_student)
+print('яблука в кошику:',  apple_v_kohk)
