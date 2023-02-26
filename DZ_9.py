65f04c7aca1706d80ce5224f087dbb1335284806
bogdansevcenko <lysik8888@gmail.com>
2023-02-26 19:35:59 +0200

    Домашка-9

---
+++ DZ_9.py
@@ -0,0 +1,18 @@
+a = input('slovo:')
+print('3_smvol/rydu:', a[2])
+b = a
+print('pered/ostaniy/smvol/rydu:', a[-2])
+c = a
+print('perhi_5_smvol/rydu:', a[:5])
+d = a
+print('vesy/rydok/krim_2_ostanyx', d[0:-2])
+e = a
+print('vsi/smvol/parni/index:', e[::2])
+f = a
+print('vsi/smvol/neparny/indeks:', f[1::2])
+g = a
+print('vsi/smvol/v_zvoroti:', g[::-1])
+h = a
+print('vsi/smvol/rydu/cherez_1:', h[::-2])
+i = a
+print('dovgena_rydu:', len(i))
